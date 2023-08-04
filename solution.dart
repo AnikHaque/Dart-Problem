a3d36dc8f375497f9cabb97aba8996e23a8c8580
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-04 23:10:51 +0600

    Create solution.dart

---
+++ solution.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map<String, dynamic> person = {
+    "PersonID": 1,
+    "Name": "John Doe",
+    "Age": 30,
+    "Salary": 50000,
+    "Gender": "Male",
+  };
+
+  print("Salary: ${person["Salary"]}");
+}
