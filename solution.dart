89dca19a88b6232c50c610918d4870edda633c38
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-05 14:24:47 +0600

    Update solution.dart

--- solution.dart
+++ solution.dart
@@ -1,3 +1,8 @@
+// Create a Map representing persons with key-value pairs. The keys are "PersonID,"
+// "Name," "Age," "Salary," and "Gender." Only the values for "PersonID,"
+// "Age," and "Salary" will be numbers. Print out the "Salary" value from the map.
+
+// solution : 
 void main() {
   Map<String, dynamic> person = {
     "PersonID": 1,
